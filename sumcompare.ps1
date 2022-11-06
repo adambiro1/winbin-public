57e4b93bb647d32b1aed8cc4a68ff77325e4b9b3
adambiro1 <88846661+adambiro1@users.noreply.github.com>
2022-11-06 21:01:05 +0100

    Add files via upload

--- sumcompare.ps1
+++ sumcompare.ps1
@@ -3,6 +3,17 @@ param([string]$p1)
 if($p1) {
     $checksum = Get-Clipboard
     #TODO check if hex is valid
+    $valid = foreach($i in [char]'A'..[char]'F' && 0..9) {$i}
+    $ascii = foreach($i in 33..126) {"$([char]$i)"}
+    foreach($i in $ascii) { 
+        if($i -contains $valid) {
+            continue
+        }
+        if($i -contains $checksum) {
+            "Checksum in clipboard is not valid"  #TODO 
+            Exit
+        }
+    }
     $isoHash = Get-FileHash $p1 -Algorithm sha256
     if ($isoHash.Hash -eq $checksum.ToUpper()) {
         "Checksum is valid."
