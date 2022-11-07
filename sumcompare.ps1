255b3decfe2d633e2978128dc6aa2147459a30a6
adambiro1 <88846661+adambiro1@users.noreply.github.com>
2022-11-07 19:46:15 +0100

    add checksum validation

--- sumcompare.ps1
+++ sumcompare.ps1
@@ -1,17 +1,30 @@
-#check if checksum sha256 is valid
+<#
+    .SYNOPSIS
+        Check if checksum is valid
+    
+    .DESCRIPTION  
+        
+    .NOTES
+        This PowerShell script compare sha256sum. 
+        You need to specify file as first argument,
+        and copy checksum for reference to clipboard.
+    .LINK
+        https://github.com/adambiro1/winbin
+#>
 param([string]$p1)
 if($p1) {
+    if (!(Test-Path -Path $p1 -PathType Leaf)){
+        "$p1 does not exist."
+        exit
+    }
     $checksum = Get-Clipboard
-    #TODO check if hex is valid
     $valid = foreach($i in [char]'A'..[char]'F' && 0..9) {$i}
     $ascii = foreach($i in 33..126) {"$([char]$i)"}
     foreach($i in $ascii) { 
-        if($i -contains $valid) {
-            continue
-        }
-        if($i -contains $checksum) {
-            "Checksum in clipboard is not valid"  #TODO 
-            Exit
+        if($valid -contains $i) { continue }
+        if($checksum.ToUpper().ToCharArray() -contains $i) {
+            "Checksum in clipboard is not valid"   
+            exit
         }
     }
     $isoHash = Get-FileHash $p1 -Algorithm sha256
